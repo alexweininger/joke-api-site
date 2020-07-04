31f94659197670b1c990dcaf90ba162c509b82b5
alexweininger <alex.weininger@live.com>
2020-07-04 13:19:30 -0700

    fix

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const joke = document.querySelector('#joke');
+async function getJoke() {
+    const joke = document.querySelector('#joke');
 
-if (joke) {
-    let response = await fetch("https://joke-api.azurewebsites.net/api/joke/", {
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "max-age=0",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "cross-site",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
-        },
-        "referrer": "https://github.com/alexweininger/joke-api",
-        "referrerPolicy": "no-referrer-when-downgrade",
-        "body": null,
-        "method": "GET",
-        "mode": "cors",
-        "credentials": "omit"
-    });
-    let jokeJson = JSON.parse(response);
+    if (joke) {
+        let response = await fetch("https://joke-api.azurewebsites.net/api/joke/", {
+            "headers": {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "max-age=0",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "cross-site",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1"
+            },
+            "referrer": "https://github.com/alexweininger/joke-api",
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": null,
+            "method": "GET",
+            "mode": "no-cors",
+            "credentials": "omit"
+        });
+        console.log(response);
+        let jokeJson = JSON.parse(response);
 
-    joke.innerHTML = jokeJson.setup;
+        joke.innerHTML = jokeJson.setup;
+    }
 }
+
+getJoke();
