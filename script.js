0ea1870279ad81831e1900ef5557f95641caadb0
alexweininger <alex.weininger@live.com>
2020-07-04 17:56:13 -0700

    Joke request working

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 async function getJoke() {
-    const joke = document.querySelector('#joke');
+    const setup = document.querySelector('#setup');
+    const punchline = document.querySelector('#punchline');
 
-    if (joke) {
-        let response = await fetch("https://joke-api.azurewebsites.net/api/joke/", {
-            "headers": {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "max-age=0",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "cross-site",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1"
-            },
-            "referrer": "https://github.com/alexweininger/joke-api",
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": null,
-            "method": "GET",
-            "mode": "no-cors",
-            "credentials": "omit"
+    if (setup) {
+        const response = await fetch("https://joke-api.azurewebsites.net/api/joke/", {
+            method: "GET",
+            credentials: "same-origin"
         });
-        console.log(response);
-        let jokeJson = JSON.parse(response);
-
-        joke.innerHTML = jokeJson.setup;
+        const jokeJson = await response.json();
+        setup.innerHTML = jokeJson.setup;
+        punchline.innerHTML = jokeJson.punchline;
     }
 }
 
